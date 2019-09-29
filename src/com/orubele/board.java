70b11453bc8e8e6b1def23f5b51d2e595b19ea2d
Emmanuel Orubele <blontf@gmail.com>
2019-09-28 23:41:04 -0400

    Update to TicTactoe game

--- src/com/orubele/board.java
+++ src/com/orubele/board.java
@@ -6,37 +6,87 @@ public class board {
     private int rowSize;
     private String board[][];
 
+
+    // board constructor that accepts two int argument.
     public board(int columnSize, int rowSize) {
         this.columnSize = columnSize;
         this.rowSize = rowSize;
 
     }
 
+    /**
+     * Method Name: CreateBoard
+     * <p>
+     * Parameters: null
+     * <p>
+     * Function: Creates a board for the tictactoe game by checking
+     * <p>
+     * if the column size and row size has been set.
+     */
 
     public void createBoard() {
-        System.out.println("I have created the board but it is empty");
+        if (this.getColumnSize() > 0 && this.getRowSize() > 0) {
+
+            this.board = new String[this.getColumnSize()][this.getRowSize()];
+            System.out.println("Board has been created" + this.board);
+
+            for (int columncounter = 0; columncounter < this.getColumnSize(); columncounter++) {
+                for (int rowcounter = 0; rowcounter < this.getRowSize(); rowcounter++) {
+                    System.out.print(this.board[columncounter][rowcounter]);
+                }
+
+            }
+
+        } else {
+
+            System.out.print("Your board size was not set please try again");
+
+        }
 
     }
 
-    public int getRowSize() {
-        return this.rowSize;
-
-    }
 
     public int getColumnSize() {
-        return this.columnSize;
+
+        return columnSize;
     }
 
+​
+
+        ​
+
+    public void setColumnSize(int columnSize) {
+
+        this.columnSize = columnSize;
+
+    }
+
+        ​
+
+                ​
+
+
+                ​
+
+    public int getRowSize() {
+
+        return rowSize;
+
+    }
+
+        ​​
+
+                ​
 
     public void setRowSize(int rowSize) {
+
         this.rowSize = rowSize;
 
     }
 
-    public void setColumnSize(int columnSize) {
-        this.columnSize = columnSize;
-    }
+        ​
 
+                ​
 
     public String[][] getBoard() {
 
@@ -44,6 +94,10 @@ public class board {
 
     }
 
+        ​
+
+                ​
+                ​
 
     public void setBoard(String[][] board) {
 
@@ -51,6 +105,7 @@ public class board {
 
     }
 
-
-
+        ​
 }
+
+
